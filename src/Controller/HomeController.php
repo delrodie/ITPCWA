d0bd538d865b2b12a71c27a1f512e887b2a71f22
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-12-15 14:55:26 +0000

    Frontend Accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\SlideRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private SlideRepository $slideRepository
+    )
+    {
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'slides' => $this->slideRepository->findBy(['statut' => true], ['id'=>'DESC']),
         ]);
     }
 }
