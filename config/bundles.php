5197195cfcc131c6848d73fe9043290fd929ae87
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2022-12-20 18:46:25 +0000

    Installation KnpTimeBundle

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
