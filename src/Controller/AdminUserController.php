b588fd6467fd5330c9fede37529ab880fdac5fa4
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-01-13 19:15:55 +0000

    Backend User

---
+++ src/Controller/AdminUserController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Flasher\Prime\Flasher;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/user')]
+class AdminUserController extends AbstractController
+{
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher, private Flasher $flasher
+    )
+    {
+    }
+
+    #[Route('/', name: 'app_admin_user_index', methods: ['GET', 'POST'])]
+    public function index(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $this->passwordHasher->hashPassword($user, $user->getPassword())
+            );
+            $userRepository->save($user, true);
+
+            $this->flasher
+                ->create('notyf')
+                ->addSuccess("L'utilisateur {$user->getUserIdentifier()} a été ajouté avec succès!");
+
+            return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('admin_user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $this->passwordHasher->hashPassword($user, $user->getPassword())
+            );
+            $userRepository->save($user, true);
+
+            $this->flasher
+                ->create('notyf')
+                ->addSuccess("L'utilisateur {$user->getUserIdentifier()} a été ajouté avec succès!");
+
+            return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
