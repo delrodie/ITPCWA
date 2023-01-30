7327526c92571d3d81b16f16aac07a7ca4943f2b
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2023-01-30 19:22:10 +0000

    Frontend Equipe

--- src/Controller/Frontend/FrontendEquipeController.php
+++ src/Controller/Frontend/FrontendEquipeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Frontend;
 
+use App\Services\GestionCache;
+use Flasher\Prime\Flasher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/team')]
 class FrontendEquipeController extends AbstractController
 {
+    public function __construct(Private GestionCache $gestionCache, private Flasher $flasher)
+    {
+    }
+
     #[Route('/{_locale}/', name: 'app_frontend_equipe_index')]
     public function index($_locale): Response
     {
         return $this->render('frontend/equipes.html.twig',[
             'locale' => $_locale,
-            'active' => "presentation"
+            'active' => "presentation",
+            'equipes' => $this->gestionCache->cacheEquipe($_locale)
+        ]);
+    }
+
+    #[Route('/{_locale}/{slug}', name: 'app_frontend_equipe_show')]
+    public function show($_locale, $slug)
+    {
+        return $this->render('frontend/equipe_membre.html.twig',[
+            'locale' => $_locale,
+            'active' => "presentation",
+            'equipe' => $this->gestionCache->cacheEquipeItem($_locale, $slug)
         ]);
     }
 }
